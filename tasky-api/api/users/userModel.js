9130d3738b7483b8e935dd78c9a5c8507892feb7
John Doe <johndoe@example.com>
2026-05-17 20:05:13 +0100

    Salt and Hash

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -1,10 +1,31 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
   password: {type: String, required: true }
+});
+
+UserSchema.methods.comparePassword = async function (passw) { 
+    return await bcrypt.compare(passw, this.password); 
+};
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function() {
+  const saltRounds = 12;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+    } catch (error) {
+      throw new Error(error);
+    }
+  } 
 });
 
 export default mongoose.model('User', UserSchema);
