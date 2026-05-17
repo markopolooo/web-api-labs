3209c5d969937fc10c7a9473ab7414b14c53400c
John Doe <johndoe@example.com>
2026-05-17 19:55:01 +0100

    Register and Authenticate Users

--- tasky-api/api/users/index.js
+++ tasky-api/api/users/index.js
@@ -1,19 +1,31 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 import User from './userModel';
 
 const router = express.Router(); 
 
+// Get all users
 router.get('/', async (req, res) => {
     const users = await User.find();
     res.status(200).json(users);
 });
 
-router.post('/', async (req, res) => {
-    await User(req.body).save();
-    res.status(201).json({
-        code: 201,
-        msg: 'Successful created new user.',
-    });
-});
+// register(Create)/Authenticate User
+router.post('/', asyncHandler(async (req, res) => {
+    if (req.query.action === 'register') {
+        await User(req.body).save();
+        res.status(201).json({
+            code: 201,
+            msg: 'Successful created new user.',
+        });
+    } else {
+        const user = await User.findOne(req.body);
+        if (!user) {
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+        }
+    }
+}));
 
 export default router;
